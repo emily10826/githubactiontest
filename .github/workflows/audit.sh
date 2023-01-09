72939df75c3ca26493cc81ccbca7eb81b4ac8d58
emily10826 <61948464+emily10826@users.noreply.github.com>
2023-01-09 10:34:30 -1000

    Update audit.sh

--- .github/workflows/audit.sh
+++ .github/workflows/audit.sh
@@ -6,10 +6,16 @@ for dir in $MODULE_DIR/*/*; do
     log_check=$(find $dir -mindepth 1 -name "*.log" -type f | wc -l)
     readme_check=$(find $dir -mindepth 1 -name "README.md" -type f | wc -l)
 
-    if [[ $manifest_check -eq 1 ]] && [[ $log_check -ge 3 ]] && [[ $readme_check -eq 1 ]]; then
-        echo "ok"
+    # if [[ $manifest_check -eq 1 ]] && [[ $log_check -ge 3 ]] && [[ $readme_check -eq 1 ]]; then
+    #     echo "ok"
+    # else
+    #     echo "Please ensure all module requirements are satisfied in $dir."
+    # fi
+
+    if [[ $manifest_check -eq 1 ]]; then
+        echo ::set-output name=manifest-check-output::"$pass"
     else
-        echo "Please ensure all module requirements are satisfied in $dir."
+        echo ::set-output name=manifest-check-output::"$fail"
     fi
     
 done
