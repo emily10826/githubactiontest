0b870035629e6c27eb4e777359daa0958a852abe
emily10826 <61948464+emily10826@users.noreply.github.com>
2023-01-09 12:42:27 -1000

    Update audit.sh

--- .github/workflows/audit.sh
+++ .github/workflows/audit.sh
@@ -4,16 +4,25 @@ MODULE_DIR='uploading_custom_modules/helm-charts/filebeat/configs/custom_mods'
 for dir in $MODULE_DIR/*/*; do
     manifest_check=$(find $dir -name "manifest.yml" -type f | wc -l)
     log_check=$(find $dir -mindepth 1 -name "*.log" -type f | wc -l)
-    readme_check=$(find $dir -mindepth 1 -name "README.md" -type f | wc -l)
-
-#     if [[ $manifest_check -eq 1 ]] && [[ $log_check -ge 3 ]] && [[ $readme_check -eq 1 ]]; then
-#         echo "ok"
-#     else
-#         echo "Please ensure all module requirements are satisfied in $dir."
-#     fi
+    readme_check=$(find $dir -maxdepth 1 -name "README.md" -type f | wc -l)
 
     if [[ $manifest_check -eq 1 ]]; then
-        echo ::error ::Missing semicolon
+        # echo ::set-output name=manifest-check-output::$pass
+        :
+    else
+        echo ::warning ::"Missing manifest.yml in $dir."
+    fi
+
+    if [[ $log_check -ge 3 ]]; then
+        :
+    else
+        echo ::warning ::"Missing at least 3 log files in $dir."
+    fi 
+
+    if [[ $readme_check -eq 1 ]]; then
+        :
+    else
+        echo ::warning ::"Missing README.md in $dir."
     fi
     
 done
